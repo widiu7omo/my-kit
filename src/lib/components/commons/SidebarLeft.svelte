5307699839f2faab0159b7ea780818d1492a041a
Danar Widi <widiu7omo@gmail.com>
2023-01-24 10:32:24 +0800

    Change listbox

--- src/lib/components/commons/SidebarLeft.svelte
+++ src/lib/components/commons/SidebarLeft.svelte
@@ -1,36 +1,46 @@
 <script lang="ts">
-	import { ListBox, ListBoxItem } from '@skeletonlabs/skeleton';
-	import { get, writable, type Writable } from 'svelte/store';
-	// Only server svelte kit avaiable
 	import { page } from '$app/stores';
 	import AppBrand from './AppBrand.svelte';
-	import { goto } from '$app/navigation';
 	import HomeIcon from '../icons/HomeIcon.svelte';
 	import UserIcon from '../icons/UserIcon.svelte';
 	import SearchIcon from '$lib/components/icons/SearchIcon.svelte';
 	import CreditCardIcon from '$lib/components/icons/CreditCardIcon.svelte';
-	import SidebarLeftItem from './SidebarLeftItem.svelte';
-	const storeSingle: Writable<string> = writable($page.url.pathname);
-	const handleClick = () => {
-		const selectedMenu = get(storeSingle);
-		goto(selectedMenu);
-	};
+	import { classNames } from '$lib/helpers/ui';
+	$: classesActive = (href: string) => (href === $page.url.pathname ? '!bg-primary-500' : '');
 </script>
 
 <div id="sidebar-left" class="hidden lg:block bg-surface-500/5 w-56 p-4 h-full">
 	<AppBrand />
-	<ListBox selected={storeSingle}>
-		<SidebarLeftItem icon={HomeIcon} on:click={handleClick} value="/">
-			<span class="text-sm font-medium">Dashboard</span>
-		</SidebarLeftItem>
-		<SidebarLeftItem icon={UserIcon} on:click={handleClick} value="/users">
-			<span class="text-sm font-medium">Users</span>
-		</SidebarLeftItem>
-		<SidebarLeftItem icon={CreditCardIcon} on:click={handleClick} value="/subscriptions">
-			<span class="text-sm font-medium">Subscriptions</span>
-		</SidebarLeftItem>
-		<SidebarLeftItem icon={SearchIcon} on:click={handleClick} value="/settings">
-			<span class="text-sm font-medium">Settings</span>
-		</SidebarLeftItem>
-	</ListBox>
+	<nav class="list-nav">
+		<ul>
+			<li>
+				<a href="/" class={classNames(classesActive('/'), '!py-1')}
+					><span class="badge"><HomeIcon /></span><span class="flex-auto text-sm font-medium"
+						>Dashboard</span
+					></a
+				>
+			</li>
+			<li>
+				<a href="/users" class={classNames(classesActive('/users'), '!py-1')}
+					><span class="badge"><UserIcon /></span><span class="flex-auto text-sm font-medium"
+						>Users</span
+					></a
+				>
+			</li>
+			<li>
+				<a href="/subscriptions" class={classNames(classesActive('/subscriptions'), '!py-1')}
+					><span class="badge"><CreditCardIcon /></span><span class="flex-auto text-sm font-medium"
+						>Subscriptions</span
+					></a
+				>
+			</li>
+			<li>
+				<a href="/settings" class={classNames(classesActive('/settings'), '!py-1')}
+					><span class="badge"><SearchIcon /></span><span class="flex-auto text-sm font-medium"
+						>Settings</span
+					></a
+				>
+			</li>
+		</ul>
+	</nav>
 </div>
